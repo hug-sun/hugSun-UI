fd6571fe74f8598708b29eb3f4470cfeed0c0757
ImJustAMan <t912272092@sina.com>
2021-03-26 11:20:37 +0800

    second commit for init project

--- src/Foo/__tests__/index.spec.tsx
+++ src/Foo/__tests__/index.spec.tsx
@@ -4,10 +4,9 @@ import Foo from '..';
 
 describe('Foo', () => {
   it('Foo is h1 tag', () => {
-    const wrapper = mount(<Foo title="title"/>);
+    const wrapper = mount(<Foo title="title" />);
 
     console.dir(wrapper.html());
     expect(wrapper).toBeTruthy();
-    expect(wrapper).toMatchSnapshot();
   });
 });
