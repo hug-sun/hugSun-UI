85c2a68d677a1d7e7060059fc065391898d2cd30
ImJustAMan <t912272092@sina.com>
2021-03-29 16:00:12 +0800

    docs(.umirc): change logo and favicon try to fix logo not found

--- .umirc.ts
+++ .umirc.ts
@@ -3,8 +3,14 @@ import { defineConfig } from 'dumi';
 
 export default defineConfig({
   title: 'hugSun-UI',
-  favicon: '/images/favicon.ico',
-  logo: '/images/logo.png',
+  favicon:
+    process.env.NODE_ENV === 'production'
+      ? '/hugSun-UI'
+      : '' + '/images/favicon.ico',
+  logo:
+    process.env.NODE_ENV === 'production'
+      ? '/hugSun-UI'
+      : '' + '/images/logo.png',
   outputPath: 'docs-dist',
   mode: 'site',
   history: {
