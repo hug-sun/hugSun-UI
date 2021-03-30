9c3bdcaebae2a5366e2d6e1cb0350bdcb17ec1f0
ImJustAMan <t912272092@sina.com>
2021-03-30 11:40:52 +0800

    feat(mobile): add dumi-theme-mobile for mobile develop

--- .umirc.ts
+++ .umirc.ts
@@ -5,15 +5,15 @@ export default defineConfig({
   title: 'hugSun-UI',
   favicon:
     (process.env.NODE_ENV === 'production' ? '/hugSun-UI' : '') +
-    '/images/favicon.ico',
+    '/images/favicon.ico', // 后续再找一下有没有其他的方案 现在这个方案部署很难受 logo同理
   logo:
     (process.env.NODE_ENV === 'production' ? '/hugSun-UI' : '') +
     '/images/logo.png',
-  outputPath: 'docs-dist',
+  outputPath: 'docs-dist', // 输出目录
   mode: 'site',
   history: {
-    type: 'hash',
+    type: 'hash', // 打包后
   },
-  publicPath: process.env.NODE_ENV === 'production' ? '/hugSun-UI/' : '/',
-  hash: true,
+  publicPath: process.env.NODE_ENV === 'production' ? '/hugSun-UI/' : '/', // 静态资源地址 ./其实也可以
+  hash: true, // 这块是静态资源会标记hash 并不是路由中的hash 不要弄混了
 });
