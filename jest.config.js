30d1d7c33ed869f29a834a58e8fb74b1c37ee7a4
ImJustAMan <t912272092@sina.com>
2021-03-31 09:35:33 +0800

    chore(jest): change umi-test to jest

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,13 @@
 module.exports = {
-  setupFiles: ["./scripts/setupTests.js"],
-  testMatch: [
-    '**/__tests__/**/*.spec.[jt]s?(x)',
-    '**/tests/**/*.spec.[jt]s'
-  ]
-}
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json'],
+  setupFiles: ['./scripts/setupTests.js'],
+  testMatch: ['**/__tests__/**/*.spec.[jt]s?(x)', '**/tests/**/*.spec.[jt]s'],
+  verbose: true,
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': '@umijs/test/helpers/transformers/javascript.js',
+    '^.+\\.(css|less|sass|scss|stylus)$':
+      '@umijs/test/helpers/transformers/css.js',
+    '^(?!.*\\.(js|jsx|ts|tsx|css|less|sass|scss|stylus|json)$)':
+      '@umijs/test/helpers/transformers/file.js',
+  },
+};
